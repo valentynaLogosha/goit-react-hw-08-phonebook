7c6cc8d93cde280648b57c64eb92d7a469fd2079
valentynaLogosha <cristianyshka@gmail.com>
2023-09-15 00:33:59 +0300

    added styles and cover

--- src/components/PhoneIcons/PhoneIcons.jsx
+++ src/components/PhoneIcons/PhoneIcons.jsx
@@ -13,8 +13,8 @@ const PhoneIcons = () => {
       const windowHeight = window.innerHeight;
       const minSize = 1;
       const maxSize = 20;
-      const minDelay = 500;
-      const maxDelay = 3000;
+      const minDelay = 2000; // Збільшена затримка
+      const maxDelay = 5000; // Збільшена затримка
       const minOpacity = 0.2;
       const maxOpacity = 1;
 
@@ -22,7 +22,7 @@ const PhoneIcons = () => {
         let left = Math.random() * windowWidth;
         let top = Math.random() * windowHeight;
         let size = Math.random() * (maxSize - minSize) + minSize;
-        let delay = Math.random() * (maxDelay - minDelay) + minDelay;
+        let delay = Math.random() * (maxDelay - minDelay) + minDelay; // Збільшена затримка
         let opacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
 
         phone.style.left = left + 'px';
@@ -39,14 +39,14 @@ const PhoneIcons = () => {
       document.body.appendChild(phone);
     };
 
-    let numPhone = 3;
+    let numPhone = 2; //  кількість іконок
 
     for (let i = 0; i < numPhone; i++) {
       createPhone();
     }
   }, []);
 
-  return;
+  return null;
 };
 
 export default PhoneIcons;
